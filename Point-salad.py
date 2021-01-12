ecd1b4e1db0bb74799967188c84f655af8c2bebf
mcgillJay <mcgillJay@users.noreply.github.com>
2021-01-11 19:05:31 -0500

    Add files via upload

---
+++ Point-salad.py
@@ -0,0 +1,40 @@
+#point salad game//
+Import Math.random
+
+playerNum = input "how many players?(2 to 6)"
+if 2>playerNum>6
+cardNum = playerNum * 6
+#for every player 3 of each veggie in play
+
+class Lettuce:
+    card =  [[pepper, 2],
+
+
+
+    ]
+class Tomato:
+    card = [
+
+    ]
+
+class Onion:
+    card = [
+
+    ]
+
+class Cabbage:
+    card = [[Tomato, pepper, 5]
+
+    ]
+
+class Pepper:
+    card = [
+
+    ]
+
+class Carrot:
+    card = [[Tomato, Lettuce, 5],[cabbage, 2],[cabbage, cabbage, cabbage, 8], [cabbage, 2, Tomato, 2, lettuce, -4], [least, cabbage, 7], [cabbage, 2, lettuce, 1, Carrot, -2]
+
+    ]
+
+
